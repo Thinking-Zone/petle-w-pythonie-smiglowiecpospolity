97d20104b769696352deb891f7b0d13d69bab24d
smiglowiecpospolity <sofiadk123@gmail.com>
2024-11-08 14:34:31 +0100

    Update zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,14 @@
+suma = 0
+for liczba_nieparzysta in range(1, 100):
+    if liczba_nieparzysta % 2 != 0:
+        suma += liczba_nieparzysta
+    #spraw aby program pokazał sumę
+    
+    
+    
+    
+    
+    
+    
+    
+    #odp print(suma) :P
